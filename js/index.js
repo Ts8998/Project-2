6286d8138dcbfbe5feaf88ceedd90531aa213a15
Ts8998 <43605948+Ts8998@users.noreply.github.com>
2019-07-31 01:42:12 +0300

    Adding functionality

--- js/index.js
+++ js/index.js
@@ -166,3 +166,43 @@ tabs.addEventListener("click", function(event) {
     });
   }
 });
+
+
+// *********************Countdown****************
+function countdownClock() {
+  var now = new Date();
+  var eventDate = new Date(2019, 8, 5, 0, 0, 0);
+
+  var currentTime = now.getTime();
+  var eventTime = eventDate.getTime();
+
+  var remTime = eventTime - currentTime;
+
+  var s = Math.floor(remTime / 1000);
+  var m = Math.floor(s / 60);
+  var h = Math.floor(m / 60);
+  var d = Math.floor(h / 24);
+
+  h %= 24;
+  m %= 60;
+  s %= 60;
+
+  if (h < 10) {
+    h = "0" + h;
+  }
+  if (m < 10) {
+    m = "0" + m;
+  }
+  if (s < 10) {
+    s = "0" + s;
+  }
+
+  document.querySelector(".days").innerText = d;
+  document.querySelector(".hours").innerText = h;
+  document.querySelector(".minutes").innerText = m;
+  document.querySelector(".seconds").innerText = s;
+
+  setTimeout(countdownClock, 1000);
+}
+
+countdownClock();
